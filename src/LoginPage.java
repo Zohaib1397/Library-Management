7bcaa7117d34902ab7de8387658408c6ddf55a56
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2023-05-20 22:09:28 +0500

    Added Login form and User class

---
+++ src/LoginPage.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class LoginPage extends JFrame {
+    LoginPage(){
+        setSize(400,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setTitle("Login Page");
+
+    }
+    public static void main(String[] args){
+        new LoginPage();
+    }
+}
